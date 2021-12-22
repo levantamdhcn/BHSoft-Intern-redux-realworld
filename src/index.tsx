385e08f0da4968a8aedd0c78efda6d57fd5017e0
tamlv.bhsoft <tamlv.bhsoft@gmail.com>
2021-12-22 11:53:20 +0700

    change base url

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import persistor from './stores/stores'
 import axios from 'axios';
 
-axios.defaults.baseURL="http://localhost:5000/"
+axios.defaults.baseURL="https://conduit-mern-app.herokuapp.com/"
 
 ReactDOM.render(
   <Provider store={stores}>
